5d6a6f77a82e07129742223597ebbcadc45e9442
Rodrigo Santos <didigorr@gmail.com>
2021-11-01 10:02:15 -0300

    versão final do código

---
+++ src/index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
+const { send } = require('process');
+const { typeOf } = require('react-is');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({error: "user not found"});
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+  
+  const userAlreadyExistis = users.some(
+    (user) => user.username === username
+  );
+
+  if(userAlreadyExistis) {
+    return response.status(400).json({error: "User already exixts"});
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if(!todo) {
+    return response.status(404).json({error: 'Invalid ID'});
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+  
+  return response.status(201).json(todo);
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+  
+  const todoDone = user.todos.find(todo => todo.id === id);
+
+  if(!todoDone) {
+    return response.status(404).json({error: 'Invalid ID'});
+  }
+
+  user.todos.find(todo => todo.id === id).done = true;
+  
+  return response.status(200).json(todoDone);
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: 'Invalid ID'});
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).json(user.todos);
+});
+
+module.exports = app;
